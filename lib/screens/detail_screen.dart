ace284c8e4b5b5689beb3aaaa0c9fe7cb47f47c9
BintangCoder <bintangilham120@gmail.com>
2025-11-26 10:44:05 +0700

    Terimakasih Cinta

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -15,18 +15,80 @@ class _DetailScreenState extends State<DetailScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.wisataModel.nama),
-      ),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              // GAMBAR UTAMA DAN TOMBOL BACK (TUMPUKAN)
+              Stack(
+                children: [
+                  // Gambar Utama
+                  Image.asset(
+                    widget.wisataModel.gambarUtama,
+                    fit: BoxFit.cover,
+                    width: double.infinity,
+                    height: 300,
+                  ),
+                  
+                  // Tombol Back
+                  Container(
+                    margin: const EdgeInsets.all(16),
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.white70
+                    ),
+                    child: IconButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      }, 
+                      icon: const Icon(Icons.arrow_back)
+                    )
+                  )
+                ],
+              ),
+              // NAMA DAN TOMBOL LOVE
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    // Nama
+                    Text(
+                      widget.wisataModel.nama,
+                      style: const TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold
+                      ),
+                    ),
+                    // Tombol Love
+                    IconButton(
+                      onPressed: () {}, 
+                      icon: Icon(
+                        Icons.favorite,
+                        color: Colors.red,
+                      )
+                    )
+                  ],
+                ),
+              )
+      
+          // ALAMAT
+      
+          // JAM BUKA
+      
+          // INSTAGRAM
+      
+          // DESKRIPSI
+      
+          // GALERI GAMBAR (SCROLLABLE)
+           ],
+          ),
+        ),
+      )
     );
   }
 }
-
-
-
-
-
-
+       
 
 
 
